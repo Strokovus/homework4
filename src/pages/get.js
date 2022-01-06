85ba52d5c0c10c90f25ffee20c31bf254794956d
Strokovus <geromewong.20@ichat.sp.edu.sg>
2022-01-07 07:43:01 +0800

    my-app

---
+++ src/pages/get.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export class Getform extends Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+          Title: '',
+          Content: '',
+          Postid: '',
+      }
+
+  }
+
+  retrievalHandler = e => {
+      axios.get("http://localhost:8082/posts/", {
+      })
+          .then(response => {
+              console.log(response)
+              alert("Retrieved!")
+              this.setState({ Title: response.data[0].title });
+              this.setState({ Content: response.data[0].content });
+          }).catch(error => {
+              console.log(error)
+          })
+  }
+
+  render() {
+      const { Title, Content} = this.state
+      return (
+          <>
+              <p>Get Request (gets the latest post)</p>
+              <button onClick={this.retrievalHandler}>
+                        Latest Post
+                    </button>
+              <h2> {Title} </h2>
+              <p>{Content}</p>
+          </>
+      )
+  }
+}
+
+export default Getform
